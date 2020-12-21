c19a093268bf206797329b4de641d2dd3d329385
Luis Javier López Arredondo <ljDevelopment.6.28@gmail.com>
2020-12-21 20:02:21 +0100

    Portfolio in two columns wider, one in narrower (flex columns wrap)

--- includes/footer.js
+++ includes/footer.js
@@ -4,6 +4,6 @@ document.body.innerHTML +=
 	'<footer id=\'foot\'>'
 	+ '<input type="button" id="goUp" onclick="resetScroll()" value="Up" />'
 		+ '<span>'
-		+ '&copy; <script>document.write(new Date().getFullYear());</script> Luis Javier López Arredondo'
+		+ '&copy;' + new Date().getFullYear() + ' Luis Javier López Arredondo'
 	+ '</span>'
 	+ '<footer>';
